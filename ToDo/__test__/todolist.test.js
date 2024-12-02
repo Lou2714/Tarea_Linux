697be1ab2d743db82f5e0a167e3733762c41da88
Lourdes Jiménez <00090220@uca.edu.sv>
2024-12-01 18:39:46 -0600

    :beers: Modify pipelines

--- ToDo/__test__/todolist.test.js
+++ ToDo/__test__/todolist.test.js
@@ -36,25 +36,19 @@ describe('Probando la To Do List', ()=> {
   test('Debería marcar la tarea como completada', () => {
     const toDoInput = document.getElementById('todo_input');
     const containerToDo = document.getElementById('toDo_container');
-
     // Simular entrada en el input
     toDoInput.value = 'Comprar queso';
-
     // Añadir tarea al DOM pasando los elementos correctamente
     addTaskToToDoList(toDoInput, containerToDo);
-
     // Verificar si el botón de completar existe
     const btnCheck = containerToDo.querySelector('.btn_actions--complete');
     if (!btnCheck) {
       throw new Error('El botón de completar no se ha encontrado');
     }
-  
     // Obtener la tarea (el párrafo con la descripción)
     const paragraph = containerToDo.querySelector('.description_task');
-  
     // Simular clic en el botón de completar
     btnCheck.click();
-  
     // Verificar que la clase 'complete_task' se ha añadido
     expect(paragraph.classList.contains('complete_task')).toBe(true);
   });
